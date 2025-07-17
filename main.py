406a0a9e6540e2f7e4b249ccc1f9d9e776382851
Ahmed Gamal <ahmedgamal0005@gmail.com>
2025-07-17 17:07:10 +0300

    feat: load and add functions
    Updated load and add functions

--- main.py
+++ main.py
@@ -45,7 +45,25 @@ def return_book():
 
 
 def load_books():
-    return []
+    """Loads books from a JSON file. Returns a list of books."""
+    if not os.path.exists(BOOKS_FILE):
+        return []
+
+    try:
+        with open(BOOKS_FILE, 'r') as file:
+            books = json.load(file)
+            return books
+    except (json.JSONDecodeError, IOError):
+        print("Error: Could not load books from file.")
+        return []
+
+def save_books(books):
+    """Saves the list of books to a JSON file."""
+    try:
+        with open(BOOKS_FILE, 'w') as file:
+            json.dump(books, file, indent=4)
+    except IOError:
+        print("Error: Could not save books to file.")
 
 
 def add_book():
